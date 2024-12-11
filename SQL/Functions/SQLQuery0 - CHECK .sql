de821fc3a5ce3b3ae5d41c7554e50299bbbfc2ee
Паша Филиппов <golubchik191@gmail.com>
2024-12-11 19:52:19 +0300

    DONE GetEasterDate

--- SQL/Functions/SQLQuery0 - CHECK .sql
+++ SQL/Functions/SQLQuery0 - CHECK .sql
@@ -12,7 +12,9 @@ SET DATEFIRST 1;
 
 --DELETE FROM DaysOFF;
 
---EXEC sp_SetDaysOffFor 2025;
+EXEC sp_SetDaysOffFor 2025;
+PRINT(dbo.GetEasterDate(2025));
+PRINT(DATENAME(WEEKDAY, dbo.GetEasterDate(2025)));
 
 SELECT 
 	[date],
